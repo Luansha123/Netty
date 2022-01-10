d197ea8dcc55f63095392fb04a1065fb25b1f4cb
Luansha123 <1348549110@qq.com>
2022-01-11 03:21:08 +0800

    2022-01-11 - Inbound & Outbound Handler

--- part2/src/main/java/dark/horse/c2/p1/HelloClientTest.java
+++ part2/src/main/java/dark/horse/c2/p1/HelloClientTest.java
@@ -5,7 +5,7 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
 
 import java.net.InetSocketAddress;
 
@@ -19,7 +19,7 @@ public class HelloClientTest {
                 .handler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
-                        ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast(new StringEncoder());
                     }
                 })
                 .connect(new InetSocketAddress("localhost", 8080))
